155a0dd1a88848b3bf486a3f1297e5133e3cd1ba
yanganan <13640662396@163.com>
2017-11-25 09:58:48 +0800

    注册界面View层实现

--- app/src/main/java/com/yan/im/RegisterActivity.kt
+++ app/src/main/java/com/yan/im/RegisterActivity.kt
@@ -1,10 +1,40 @@
 package com.yan.im
 
+import com.yan.im.contract.RegisterContract
+import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.toast
+
 /**
  *  @author      : 楠GG
  *  @date        : 2017/11/25 9:45
  *  @description : 注册用户界面
  */
-class RegisterActivity: BaseActivity() {
+class RegisterActivity: BaseActivity(), RegisterContract.View {
+    override fun onUsernameError() {
+        userName.error = getString(R.string.user_name_error)
+    }
+
+    override fun onPasswordError() {
+        password.error = getString(R.string.password_error)
+    }
+
+    override fun onConfirmPasswordError() {
+        confirmPassword.error = getString(R.string.confirm_password_error)
+    }
+
+    override fun onStartRegister() {
+        showProgressDialog(getString(R.string.registering))
+    }
+
+    override fun onRegisterSuccess() {
+        hideProgressDialog()
+        finish()
+    }
+
+    override fun onRegisterFailed() {
+        hideProgressDialog()
+        toast(R.string.register_failed)
+    }
+
     override fun getLayoutId(): Int = R.layout.activity_register
 }
