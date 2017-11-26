82852742284538e7efb94cbfa9a12539baa1a366
yanganan <13640662396@163.com>
2017-11-26 23:11:37 +0800

    查询所有联系人

--- app/src/main/java/com/yan/im/model/db/IMDatabase.kt
+++ app/src/main/java/com/yan/im/model/db/IMDatabase.kt
@@ -1,7 +1,9 @@
 package com.yan.im.model.db
 
 import com.yan.im.ext.toVarargArray
+import org.jetbrains.anko.db.MapRowParser
 import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.db.select
 
 /**
  *  @author      : 楠GG
@@ -14,9 +16,21 @@ class IMDatabase {
         val instance = IMDatabase()
     }
 
+    /**
+     * 保存联系人
+     */
     fun saveContact(contact: Contact) {
         dbHelper.use {
             insert(ContactTable.NAME, *contact.map.toVarargArray())
         }
     }
+
+    /**
+     * 获取所有联系人
+     */
+    fun getAllContacts(): List<Contact> = dbHelper.use {
+            select(ContactTable.NAME).parseList(object : MapRowParser<Contact>{
+                override fun parseRow(columns: Map<String, Any?>): Contact = Contact(columns.toMutableMap())
+            })
+        }
 }
