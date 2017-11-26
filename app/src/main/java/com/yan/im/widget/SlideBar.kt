4aa76219186ef91b46b14a3d8e9f33229e0116f1
yanganan <13640662396@163.com>
2017-11-26 10:26:11 +0800

    SlideBar绘制

---
+++ app/src/main/java/com/yan/im/widget/SlideBar.kt
@@ -0,0 +1,53 @@
+package com.yan.im.widget
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+import com.yan.im.R
+import org.jetbrains.anko.sp
+
+/**
+ *  @author      : 楠GG
+ *  @date        : 2017/11/26 10:13
+ *  @description : 联系人界面右侧索引条
+ */
+class SlideBar: View {
+
+    /** 每一个字母占用的高度 */
+    var sectionHeight = 0f
+
+    private val paint = Paint()
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    init {
+        paint.apply {
+            color = resources.getColor(R.color.qq_section_text_color)
+            textSize = sp(12).toFloat()
+            //文本居中
+            textAlign = Paint.Align.CENTER
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        sectionHeight = h* 1f / SECTIONS.size
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        val x = width/2f
+        var y = sectionHeight
+        SECTIONS.forEach {
+            canvas.drawText(it, x, y, paint)
+            y += sectionHeight
+        }
+    }
+
+    companion object {
+        private val SECTIONS = arrayOf("A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+                "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+    }
+}
