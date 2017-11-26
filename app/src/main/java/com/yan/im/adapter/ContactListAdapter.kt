b349600aba2578ba35ab9784b2b520769f248ae7
yanganan <13640662396@163.com>
2017-11-26 09:53:54 +0800

    长按弹出删除好友对话框

--- app/src/main/java/com/yan/im/adapter/ContactListAdapter.kt
+++ app/src/main/java/com/yan/im/adapter/ContactListAdapter.kt
@@ -1,9 +1,11 @@
 package com.yan.im.adapter
 
 import android.content.Context
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import com.yan.im.R
 import com.yan.im.model.ContactListItem
 import com.yan.im.ui.activity.ChatActivity
 import com.yan.im.widget.ContactListItemView
@@ -29,6 +31,26 @@ class ContactListAdapter(val context: Context, private val contactList: MutableL
         val username = contactList[position].name
         //条目点击事件、跳转聊天界面
         itemView.setOnClickListener { context.startActivity<ChatActivity>("username" to username) }
+        //长按删除好友操作
+        val message = String.format(context.getString(R.string.delete_friend_message), username)
+        itemView.setOnLongClickListener {
+            AlertDialog.Builder(context)
+                    .setTitle(R.string.delete_friend_title)
+                    .setMessage(message)
+                    .setNegativeButton(R.string.cancel, null)
+                    .setPositiveButton(R.string.confirm) {dialog, which ->
+                        deleteFriend(username)
+                    }
+                    .show()
+            true
+        }
+    }
+
+    /**
+     * 删除好友
+     */
+    private fun deleteFriend(username: String) {
+
     }
 
     class ContactListItemViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView)
