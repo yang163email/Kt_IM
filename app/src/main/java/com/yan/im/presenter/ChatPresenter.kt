46aac9ed5ca614488436ecef1ce45b9cef0d6d83
yanganan <13640662396@163.com>
2017-11-27 16:00:06 +0800

    发送一条消息Model层实现

--- app/src/main/java/com/yan/im/presenter/ChatPresenter.kt
+++ app/src/main/java/com/yan/im/presenter/ChatPresenter.kt
@@ -1,6 +1,9 @@
 package com.yan.im.presenter
 
+import com.hyphenate.chat.EMClient
+import com.hyphenate.chat.EMMessage
 import com.yan.im.contract.ChatContract
+import com.yan.im.utils.EMCallBackAdapter
 
 /**
  *  @author      : 楠GG
@@ -8,7 +11,23 @@ import com.yan.im.contract.ChatContract
  *  @description : 聊天界面 P 层
  */
 class ChatPresenter(val view: ChatContract.View): ChatContract.Presenter {
-    override fun sendMessage(contact: String, message: String) {
 
+    val messages = mutableListOf<EMMessage>()
+
+    override fun sendMessage(contact: String, message: String) {
+        val emMessage = EMMessage.createTxtSendMessage(message, contact)
+        emMessage.setMessageStatusCallback(object : EMCallBackAdapter{
+            override fun onSuccess() {
+                uiThread { view.onSendMessageSuccess() }
+            }
+
+            override fun onError(p0: Int, p1: String?) {
+                uiThread { view.onSendMessageFailed() }
+            }
+
+        })
+        messages.add(emMessage)
+        view.onStartSendMessage()
+        EMClient.getInstance().chatManager().sendMessage(emMessage)
     }
 }
