f16a2b9b58b684f4fc257db301cb9d79068c1e0c
yanganan <13640662396@163.com>
2017-11-27 21:52:10 +0800

    会话界面数据加载

--- app/src/main/java/com/yan/im/adapter/ConversationListAdapter.kt
+++ app/src/main/java/com/yan/im/adapter/ConversationListAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import com.hyphenate.chat.EMConversation
 import com.yan.im.widget.ConversationListItemView
 
 /**
@@ -11,8 +12,8 @@ import com.yan.im.widget.ConversationListItemView
  *  @date        : 2017/11/27 21:23
  *  @description : 消息界面RecyclerView的adapter
  */
-class ConversationListAdapter(val context: Context): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun getItemCount(): Int = 30
+class ConversationListAdapter(val context: Context, val conversations: MutableList<EMConversation>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun getItemCount(): Int = conversations.size
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
     }
--- app/src/main/java/com/yan/im/ui/fragment/ConversationFragment.kt
+++ app/src/main/java/com/yan/im/ui/fragment/ConversationFragment.kt
@@ -1,10 +1,14 @@
 package com.yan.im.ui.fragment
 
 import android.support.v7.widget.LinearLayoutManager
+import com.hyphenate.chat.EMClient
+import com.hyphenate.chat.EMConversation
 import com.yan.im.R
 import com.yan.im.adapter.ConversationListAdapter
 import kotlinx.android.synthetic.main.fragment_conversation.*
 import kotlinx.android.synthetic.main.header.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 /**
  *  @author      : 楠GG
@@ -12,19 +16,32 @@ import kotlinx.android.synthetic.main.header.*
  *  @description : 消息fragment
  */
 class ConversationFragment: BaseFragment() {
+
+    val conversations = mutableListOf<EMConversation>()
+
     override fun getResLayoutId(): Int = R.layout.fragment_conversation
 
     override fun init() {
         super.init()
         headerTitle.text = getString(R.string.message)
         initRecyclerView()
+        loadConversations()
     }
 
     private fun initRecyclerView() {
         recyclerView.apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(context)
-            adapter = ConversationListAdapter(context)
+            adapter = ConversationListAdapter(context, conversations)
         }
     }
+
+    private fun loadConversations() {
+        doAsync {
+            val allConversations = EMClient.getInstance().chatManager().allConversations
+            conversations.addAll(allConversations.values)
+            uiThread { recyclerView.adapter.notifyDataSetChanged() }
+        }
+    }
+
 }
