d51d6db9ffe26c6edc6dc29e7703c51332b297a7
yanganan <13640662396@163.com>
2017-11-27 15:09:10 +0800

    聊天界面布局初始化

--- app/src/main/java/com/yan/im/ui/activity/ChatActivity.kt
+++ app/src/main/java/com/yan/im/ui/activity/ChatActivity.kt
@@ -1,6 +1,11 @@
 package com.yan.im.ui.activity
 
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import com.yan.im.R
+import kotlinx.android.synthetic.main.activity_chat.*
+import kotlinx.android.synthetic.main.header.*
 
 /**
  *  @author      : 楠GG
@@ -9,4 +14,34 @@ import com.yan.im.R
  */
 class ChatActivity: BaseActivity() {
     override fun getLayoutId(): Int = R.layout.activity_chat
+
+    override fun init() {
+        super.init()
+        initHeader()
+        initEditText()
+    }
+
+    private fun initEditText() {
+        edit.addTextChangedListener(object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                send.isEnabled = !s.isNullOrEmpty()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+        })
+    }
+
+    private fun initHeader() {
+        back.visibility = View.VISIBLE
+        back.setOnClickListener { finish() }
+
+        val username = intent.getStringExtra("username")
+        val titleStr = String.format(getString(R.string.chat_title), username)
+        headerTitle.text = titleStr
+    }
 }
