15f49e58e2671b2d2f37da0bab7498f399b00991
yanganan <13640662396@163.com>
2017-11-28 20:32:50 +0800

    多设备登录处理

--- app/src/main/java/com/yan/im/ui/activity/MainActivity.kt
+++ app/src/main/java/com/yan/im/ui/activity/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.yan.im.ui.activity
 
+import com.hyphenate.EMConnectionListener
+import com.hyphenate.EMError
 import com.hyphenate.chat.EMClient
 import com.hyphenate.chat.EMMessage
 import com.yan.im.R
 import com.yan.im.utils.EMMessageListenerAdapter
 import com.yan.im.utils.FragmentFactory
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 /**
  *  @author      : 楠GG
@@ -31,6 +34,20 @@ class MainActivity : BaseActivity() {
         }
 
         EMClient.getInstance().chatManager().addMessageListener(messageListener)
+        EMClient.getInstance().addConnectionListener(object : EMConnectionListener{
+            override fun onConnected() {
+
+            }
+
+            override fun onDisconnected(p0: Int) {
+                if (p0 == EMError.USER_LOGIN_ANOTHER_DEVICE) {
+                    //发生多设备登录，跳转登录界面
+                    startActivity<LoginActivity>()
+                    finish()
+                }
+            }
+
+        })
     }
 
     /**
